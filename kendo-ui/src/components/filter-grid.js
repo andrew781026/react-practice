0092ed4907ea1564c2d058f47d00f0b675980285
andrew <andrew7810262000@gmail.com>
2019-04-01 17:21:10 +0800

    add orderBy and paging

--- kendo-ui/src/components/filter-grid.js
+++ kendo-ui/src/components/filter-grid.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Grid, GridToolbar, GridColumn as Column} from '@progress/kendo-react-grid';
 import {defineFont} from '@progress/kendo-drawing/pdf';
 import {GridPDFExport} from '@progress/kendo-react-pdf';
-import {filterBy} from '@progress/kendo-data-query';
+import {filterBy, orderBy} from '@progress/kendo-data-query';
 import {sampleProducts} from '../datas/sample-products.jsx';
 
 
@@ -10,7 +10,13 @@ class FilterGrid extends React.Component {
 
     gridPDFExport;
 
-    state = {};
+    state = {
+        skip: 0,
+        take: 10,
+        sort: [
+            {field: 'ProductName', dir: 'asc'}
+        ]
+    };
 
     setFilter = () => {
 
@@ -24,10 +30,27 @@ class FilterGrid extends React.Component {
         this.setState({filter});
     };
 
+    clearFilter = () => {
+
+        const filter = undefined;
+
+        this.setState({filter});
+    };
+
+    pageChange = (event) => {
+        this.setState({
+            skip: event.page.skip,
+            take: event.page.take
+        });
+    };
+
     exportPDF = () => {
 
         // 微軟正黑體 TTF
         const Microsoft_JhengHei_Url = "http://cloud.ziti8.cn/fonts/weiruan/%E5%BE%AE%E8%BD%AF%E6%AD%A3%E9%BB%91%E4%BD%93.ttf";
+
+        // 思源黑體
+        const googleFontUrl = 'https://fonts.googleapis.com/earlyaccess/notosanskannada.css';
 
         defineFont({
             "DejaVu Sans": Microsoft_JhengHei_Url,
@@ -47,25 +70,42 @@ class FilterGrid extends React.Component {
 
     render() {
 
+        console.log(this.state.sort);
+
+        const sortedData = orderBy(sampleProducts, this.state.sort);
+        const filteredData = filterBy(sortedData, this.state.filter);
+        const pageFilteredData = filteredData.slice(this.state.skip, this.state.take + this.state.skip);
+
         const grid = (
             <Grid
-                style={{height: '420px'}}
-                data={filterBy(sampleProducts, this.state.filter)}
+                style={{height: '550px'}}
+                data={pageFilteredData}
+
+                skip={this.state.skip}
+                take={this.state.take}
+                total={filteredData.length}
+                pageable={true}
+                onPageChange={this.pageChange}
+
                 filterable
                 filter={this.state.filter}
                 onFilterChange={(e) => {
-                    this.setState({
-                        filter: e.filter
-                    });
+                    this.setState({filter: e.filter});
+                }}
+
+                sortable
+                sort={this.state.sort}
+                onSortChange={(e) => {
+                    this.setState({sort: e.sort});
                 }}
             >
                 <GridToolbar>
                     <button
                         title="Export PDF"
                         className="k-button k-primary"
-                        onClick={this.setFilter}
+                        onClick={(this.state.filter) ? this.clearFilter : this.setFilter}
                     >
-                        設定 篩選條件 ( Filter )
+                        {(this.state.filter) ? '清除 篩選條件 ( Filter )' : '設定 篩選條件 ( Filter )'}
                     </button>
                     <button
                         title="Export PDF"
@@ -75,7 +115,7 @@ class FilterGrid extends React.Component {
                         Export PDF
                     </button>
                 </GridToolbar>
-                <Column field="ProductID" title="ID" filterable={false} width="40px"/>
+                <Column field="ProductID" title="ID" filterable={false} width="70px"/>
                 <Column field="ProductName" title="Product Name"/>
                 <Column field="FirstOrderedOn" width="240px" filter="date" format="{0:d}"/>
                 <Column field="UnitPrice" width="180px" filter="numeric" format="{0:c}"/>
