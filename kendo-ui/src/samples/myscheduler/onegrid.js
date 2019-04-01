3bef7d9e7bcd023b161d0c4d32102d66bf358f67
andrew <andrew7810262000@gmail.com>
2019-04-01 23:53:00 +0800

    add main.css for common styling

--- kendo-ui/src/samples/myscheduler/onegrid.js
+++ kendo-ui/src/samples/myscheduler/onegrid.js
@@ -2,18 +2,21 @@ import React from 'react';
 
 import moment from 'moment';
 
+import './css/main.css';
+
+
 class OneGrid extends React.Component {
 
 
     render() {
 
         return (
-            <div style={{display: 'flex', flex: 1, flexDirection: 'column'}}>
+            <div className='flex flex-1 flex-col'>
                 <div style={{display: 'flex', justifyContent: 'center', flex: 1}}>
                     <span style={{fontSize: 20}}>  {moment(this.props.date).date()}</span>
                 </div>
                 <div style={{display: 'flex', flexDirection: 'row'}}>
-                    <div style={{display: 'flex', justifyContent: 'center', flex: 1}}>@</div>
+                    <div className='flex flex-1 justify-center bg-grey-dark'>@</div>
                     <div style={{display: 'flex', justifyContent: 'center', flex: 1}}>3</div>
                 </div>
             </div>
